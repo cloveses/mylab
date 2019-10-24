8e0d8ca2d0997a64b54b3132088d2c9461d7fcd8
djx <1181564186@qq.com>
2019-10-24 23:03:03 +0800

    e

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -78,48 +78,158 @@ class Queue(object):
         print(datas)
         return datas
 
-# class Stack(object):
-#     def __init__(self):
-#         # We want to initialize our Stack to be empty
-#         # (ie) Set top as null
+class Stack(object):
+    def __init__(self):
+        # We want to initialize our Stack to be empty
+        # (ie) Set top as null
+        self.__top = None
     
-#     def push(self, newData):
-#         # We want to create a node whose data is newData and next node is top
-#         # Push this new node onto the stack
-#         # Update top
+    def push(self, newData):
+        # We want to create a node whose data is newData and next node is top
+        # Push this new node onto the stack
+        # Update top
+        node = Node(newData)
+        if self.__top is None:
+            self.__top = node
+        else:
+            node.setNext(self.__top)
+            self.__top = node
     
-#     def pop(self):
-#         # Return the Node that currently represents the top of the stack
-#         # Update top
-#         # Hint: The order you implement the above 2 tasks matters, so use a temporary node
-#         #         to hold important information
-#         # Hint: Return null on a empty stack
+    def pop(self):
+        # Return the Node that currently represents the top of the stack
+        # Update top
+        # Hint: The order you implement the above 2 tasks matters, so use a temporary node
+        #         to hold important information
+        # Hint: Return null on a empty stack
+        if self.__top is None:
+            return None
+        else:
+            tmp_node = self.__top
+            self.__top = tmp_node.getNext()
+            return tmp_node.getData()
         
-#     def isEmpty(self):
-#         # Check if the Stack is empty
+    def isEmpty(self):
+        # Check if the Stack is empty
+        if not self.__top:
+            return True
+        else:
+            return False
         
-#     def printStack(self):
-#         # Loop through your stack and print each Node's data
-        
+    def printStack(self):
+        # Loop through your stack and print each Node's data
+        datas = []
+        temp = self.__top
+        while True:
+            if temp:
+                datas.append(temp.getData())
+                temp = temp.getNext()
+            else:
+                break
+        print(datas)
+        return datas
+       
 
-# def isPalindrome(s):
-#     # Use your Queue and Stack class to test wheather an input is a palendrome
-#     myStack = Stack() 
-#     myQueue = Queue()
+def isPalindrome(s):
+    # Use your Queue and Stack class to test wheather an input is a palendrome
+    myStack = Stack() 
+    myQueue = Queue()
 
-#     ## Helper function ##
-#     # print("stack data")
-#     # myStack.printStack()
+    ## Helper function ##
+    # print("stack data")
+    # myStack.printStack()
 
-#     # print("queue data")
-#     # myQueue.printQueue()
+    # print("queue data")
+    # myQueue.printQueue()
+    s = s.replace(' ', '')
+    s = s.lower()
 
-#     if True:
-#         return True:
-#     else:
-#         return False:
+    for ch in s:
+        myQueue.enqueue(ch)
+        myStack.push(ch)
 
-# def isPalindromeEC(s):
-#     # Implement if you wish to do the extra credit.
-#     return
+    ss = myStack.printStack()
+    sq = myQueue.printQueue()
+    
 
+    if ss == sq:
+        return True
+    else:
+        return False
+
+class TwoStackQueue:
+    def __init__(self):
+        self.left = Stack()
+        self.right = Stack()
+
+    def enqueue(self, newData):
+        # Create a new node whose data is newData and whose next node is null
+        # Update head and tail
+        # Hint: Think about what's different for the first node added to the Queue
+        self.left.push(newData)
+
+    
+    def dequeue(self):
+        #  Return the head of the Queue
+        #  Update head
+        #  Hint: The order you implement the above 2 tasks matters, so use a temporary node
+        #          to hold important information
+        #  Hint: Return null on a empty Queue
+        if self.left.isEmpty():
+            return None
+        else:
+            while not self.left.isEmpty():
+                self.right.push(self.left.pop())
+            res = self.right.pop()
+            while not self.right.isEmpty():
+                self.left.push(self.right.pop())
+            return res
+
+    def isEmpty(self):
+        # Check if the Queue is empty
+        return self.left.isEmpty()
+    
+    def printQueue(self):
+        datas = []
+        if not self.left.isEmpty():
+            while not self.left.isEmpty():
+                self.right.push(self.left.pop())
+            while not self.right.isEmpty():
+                temp = self.right.pop()
+                datas.append(temp)
+                self.left.push(temp)
+        print(datas)
+        return datas
+
+def isPalindromeEC(s):
+    # Implement if you wish to do the extra credit.
+    # strings = ('Hello', 'ni t I n', 'aabacabaa', '{(< >)}', '{(<<({', '344484443')
+    # s = s.replace(' ', '')
+    # s = s.lower()
+    # ss = s[::-1]
+    # if ss == s:
+    #     return True
+    # else:
+    #     return False
+    myStack = Stack() 
+    myQueue = TwoStackQueue()
+
+    ## Helper function ##
+    # print("stack data")
+    # myStack.printStack()
+
+    # print("queue data")
+    # myQueue.printQueue()
+    s = s.replace(' ', '')
+    s = s.lower()
+
+    for ch in s:
+        myQueue.enqueue(ch)
+        myStack.push(ch)
+
+    ss = myStack.printStack()
+    sq = myQueue.printQueue()
+    
+    if ss == sq:
+        return True
+    else:
+        return False
--- lab1/test_lab1.py
+++ lab1/test_lab1.py
@@ -17,44 +17,87 @@ class T0_TestingQueue(unittest.TestCase):
         self.assertEqual(q.printQueue(), [1,2,3,4])
         print("\n")
 
-
-# class T1_TestingStack(unittest.TestCase):
+class My_T0_TestingQueue(unittest.TestCase):
     
-#     def test_push_pop(self):
-#         #testing stack push operation
-#         print("\n")
-#         print ("Testing Stack: Push and Pop")
-#         s = lab1.Stack()
-#         s.push(1)
-#         s.push(2)
-#         s.pop()
-#         s.push(3)
-#         s.push(4)
-#         s.push(5)
-#         self.assertEqual(s.printStack(), [5,4,3,1])
-#         print("\n")
+    def test_empty_enqueue(self):
+        #testing the basic enqueue operation
+        print("\n")
+        print("Testing empty Queue : Empty")
+        q = lab1.Queue()
+        self.assertEqual(q.isEmpty(), True)
+        self.assertIsNone(q.dequeue())
+        data = 1
+        q.enqueue(data)
+        _data = q.dequeue()
+        self.assertEqual(q.isEmpty(), True)
+        self.assertIsNone(q.dequeue())
+        self.assertEqual(data, _data)
 
-#     def test_is_empty_false(self):
-#         #testing if queue is empty
-#         print("\n")
-#         s = lab1.Stack()
-#         s.push("4")
-#         print("return false if the stack is not empty")
-#         self.assertEqual(s.isEmpty(), False)
-#         print("\n")
+    def test_enter_dequeque(self):
+        print()
+        print("Testing enter and de...")
+        ss = 'abcde'
+        q = lab1.Queue()
+        for s in ss:
+            q.enqueue(s)
+        res = ''
+        while not q.isEmpty():
+            res += q.dequeue()
+        self.assertEqual(res, ss)
+
+
+class T1_TestingStack(unittest.TestCase):
+    
+    def test_push_pop(self):
+        #testing stack push operation
+        print("\n")
+        print ("Testing Stack: Push and Pop")
+        s = lab1.Stack()
+        s.push(1)
+        s.push(2)
+        s.pop()
+        s.push(3)
+        s.push(4)
+        s.push(5)
+        self.assertEqual(s.printStack(), [5,4,3,1])
+        print("\n")
+
+    def test_is_empty_false(self):
+        #testing if queue is empty
+        print("\n")
+        s = lab1.Stack()
+        s.push("4")
+        print("return false if the stack is not empty")
+        self.assertEqual(s.isEmpty(), False)
+        print("\n")
 
    
 
-# class T2_TestingPalindrome(unittest.TestCase):
+class T2_TestingPalindrome(unittest.TestCase):
     
-#     def test_basic_string(self):
-#         # testing with basic string
-#         print("\n")
-#         string = "hello"
-#         p = lab1.isPalindrome(string)
-#         print("The string being tested is -> ",string )
-#         self.assertEqual(p, False)
-#         print("\n")
+    def test_basic_string(self):
+        # testing with basic string
+        print("\n")
+        string = "hello"
+        p = lab1.isPalindrome(string)
+        print("The string being tested is -> ",string )
+        self.assertEqual(p, False)
+        print("\n")
+
+    def test_some_string(self):
+        print()
+        print('Testing isPalindrome...')
+        ss = ('ni t I n', 'aabacabaa', '{(<<({', '344484443', 'abcde')
+        results = (True, True, True, True, False)
+        for s, result in zip(ss, results):
+            self.assertEqual(lab1.isPalindromeEC(s), result)
+
+class My_T3_TestingPalindromeEC(unittest.TestCase):
+    def test_isPalindrome(self):
+        print()
+        print('Testing isPalindrome and isPalindromeEC')
+        for s in ('ni t I n', 'aabacabaa', '{(<<({', '344484443', 'abcde'):
+            self.assertEqual(lab1.isPalindrome(s), lab1.isPalindromeEC(s))
 
 if __name__ == '__main__':
     unittest.main()
