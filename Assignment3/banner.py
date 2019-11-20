524330c01596eb50af407ba8ed636fd6fac2625b
djx <1181564186@qq.com>
2019-11-20 14:14:48 +0800

    e

--- Assignment3/banner.py
+++ Assignment3/banner.py
@@ -1,3 +1,38 @@
 
 def make_banner(text):
     # Write code here
+    # 将所有字符改为小写
+    text = text.lower()
+    # 测试接受的参数中是存在非字母字符
+    for t in text:
+        if not 'a' <= t <= 'z':
+            # 引发错误
+            raise ValueError("All characters in text must be alphabetic.")
+    letters = []
+    # 读取banner_letters.txt文件中内容
+    with open('banner_letters.txt', 'r') as f:
+        for line in f.readlines():
+            # 去除每行的换行符并加入列表中
+            letters.append(line.strip('\n'))
+    print(len(letters))
+    # 用于保存要返回各行字符列表
+    results = []
+    # 通过获取所有字符的ascii码并减去97来获取字符在数据中的索引
+    indexes = []
+    for t in text:
+        indexes.append(ord(t) - 97)
+    # 分行获取多个字符的第0－7行符号
+    for i in range(7):
+        line = []
+        # 遍历字符序号，并获取各字符对应行的数据
+        for index in indexes:
+            line.append(letters[index * 7 + i])
+        # 各字符符号数据之间添加空格
+        line = ' '.join(line)
+        results.append(line)
+    # 各行字符数据之间添加换行符
+    results = '\n'.join(results)
+    return results
+
+print(make_banner('ebks'))
+    
--- Assignment3/convergence.py
+++ Assignment3/convergence.py
@@ -2,22 +2,50 @@ import math
 from matplotlib import pyplot as plt
 
 def taylor_sum(k, n):
+    # k值超出范围则引发错误
     if not -1 < k <= 1:
         raise ValueError('−1 < k ≤ 1')
     s = 0
-    for i in range(n):
-        s += ((-1) ** n) * ((k ** (n + 1)) / (n + 1))
+    #求和
+    for i in range(1, n+1):
+        factor = (-1) ** (n + 1)
+        num = k ** n
+        den = n
+        s += (num / den) * factor
     return s
 
-
-ns = (1,3,5,9)
+# 收集用于制作图例的数据
+legend_hdls = []
+legend_labels = []
+# 添加标题
+plt.title('Taylor sum vs ln(x+1)', fontsize='large', fontweight='bold')
+#循环绘制四条曲线
+ns = (1, 3, 5, 9) #
 for n in ns:
+    x = -0.9
     x_datas = []
     y_datas = []
-    x = -0.9
     while x <= 1:
         x_datas.append(x)
         y_datas.append(taylor_sum(x, n))
         x += 0.1
-    plt.plot(x_datas, y_datas)
+    hdl, = plt.plot(x_datas, y_datas)
+    legend_hdls.append(hdl)
+    legend_labels.append('n=%d' % n)
+
+# 绘制对比曲线
+x = -0.9
+x_datas = []
+y_datas = []
+while x <= 1:
+    x_datas.append(x)
+    y_datas.append(math.log(x+1))
+    x += 0.1
+hdl, plt.plot(x_datas, y_datas, color='black')
+legend_hdls.append(hdl)
+legend_labels.append('ln(x+1)')
+#添加图例
+plt.legend(handles=legend_hdls, labels=legend_labels)
+#保存图片
+plt.savefig("taylorsum.png")
 plt.show()
