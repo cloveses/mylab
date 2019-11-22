beb66322b05cb43a25e508918cee35781f102f4c
djx <1181564186@qq.com>
2019-11-22 23:32:54 +0800

    e

--- lot/main.py
+++ lot/main.py
@@ -1,23 +1,52 @@
+import xlrd
 import random
-staff = {'id':[], }
+
+def get_file_datas(filename,row_deal_function=None,grid_end=0,start_row=1):
+    """start_row＝1 有一行标题行；gred_end=1 末尾行不导入"""
+    """row_del_function 为每行的数据类型处理函数，不传则对数据类型不作处理 """
+    wb = xlrd.open_workbook(filename)
+    ws = wb.sheets()[0]
+    nrows = ws.nrows
+    datas = []
+    for i in range(start_row,nrows-grid_end):
+        row = ws.row_values(i)
+        # print(row)
+        if row_deal_function:
+            row = row_deal_function(row)
+        datas.append(row)
+    return datas
+
+def row_data_clean(row):
+    row = [str(r) for r in row]
+    row[0] = int(float(row[0]))
+    row[1] = int(float(row[1]))
+    for i in range(4, 14):
+        row[i] = ord(row[i]) - 64
+    return row
+datas = get_file_datas('副本测试数据.xlsx', row_data_clean)
+staff = {}
+for data in datas:
+    staff[data[0]] = data
+# staff = {'id':[], }
 # results = [['总人数',{'乘车点1人数':0, },['staffid', ]], [], ]
-results = [[0, {},[]], [], ]
+results = [[0, {},[]] for i in range(17) ]
 
 valid_roads = list(range(10, 17))
 limit_num = 80
 
 for i in range(10):
     wishes = []
-    for staffid, road in staff.items():
-        wishes.append((staffid, road))
+    for staffid, staff_data in staff.items():
+        wishes.append((staffid, staff_data[i+4]))
     free_wishes = [w for w in wishes if w[-1] <= 9]
     limit_wishes = [w for w in wishes if w[-1] > 9]
     for free_road in free_wishes:
         staffid = free_road[0]
         peoples = staff[staffid][1]
         results[i][0] += peoples
+        results[i][2].append(staffid)
         if staff[staffid][2] == '是':
-            station = staff[staffid][1]
+            station = staff[staffid][3]
             if station in results[i][1]:
                 results[i][1][station] += peoples
             else:
@@ -25,6 +54,7 @@ for i in range(10):
         del staff[staffid]
 
     random.shuffle(limit_wishes)
+    print(limit_wishes)
     for limit_road in limit_wishes:
         staffid = limit_road[0]
         peoples = staff[staffid][1]
@@ -34,10 +64,11 @@ for i in range(10):
                 continue
             else:
                 results[i][0] += peoples
+                results[i][2].append(staffid)
                 if results[i][0] >= 80:
                     del valid_roads[i]
                 if staff[staffid][2] == '是':
-                    station = staff[staffid][1]
+                    station = staff[staffid][3]
                     if station in results[i][1]:
                         results[i][1][station] += peoples
                     else:
@@ -45,10 +76,6 @@ for i in range(10):
                 del staff[staffid]
 
 print(len(staff))
-
-
-
-        
-
-    
+for i in results:
+    print(i)
 
